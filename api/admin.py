656e74030b3abedbe3de0ad665a96ddae30610f3
Dev Namdev <devnamdevcse@gmail.com>
2024-01-29 10:14:36 +0530

    add filter option

--- api/admin.py
+++ api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Song
 
 class SongAdmin(admin.ModelAdmin):
-    list_display = ('id','title', 'artist', )
+    list_display = ('id','title','categorie', 'artist', )
     search_fields = ('title', 'artist')
 
 admin.site.register(Song, SongAdmin)
--- api/models.py
+++ api/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Song(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
+    categorie = models.CharField(max_length=100, null=True, default=None)
     artist = models.CharField(max_length=100)
     audio_file = models.FileField(upload_to='audio/')
 
