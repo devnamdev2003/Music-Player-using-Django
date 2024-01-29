e4d580d27190f3c0b2f2a94aabc8f09dc881b24a
Dev Namdev <devnamdevcse@gmail.com>
2024-01-29 10:52:22 +0530

    add image

--- api/models.py
+++ api/models.py
@@ -8,6 +8,7 @@ class Song(models.Model):
     categorie = models.CharField(max_length=100, null=True, default=None)
     artist = models.CharField(max_length=100)
     audio_file = models.FileField(upload_to='audio/')
+    audio_img = models.FileField(upload_to='audio_img/')
 
     def __str__(self):
         return self.title
