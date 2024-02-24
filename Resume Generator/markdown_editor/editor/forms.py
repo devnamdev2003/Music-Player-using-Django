91762ca7cbe520c17da10ec25ce62f0ada16875f
Dev Namdev <devnamdevcse@gmail.com>
2024-02-24 08:57:35 +0530

    update

--- Resume Generator/markdown_editor/editor/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import MarkdownDocument
-
-class MarkdownForm(forms.ModelForm):
-    class Meta:
-        model = MarkdownDocument
-        fields = ['title', 'content']
--- Resume Generator/markdown_editor/editor/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class MarkdownDocument(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-
-    def __str__(self):
-        return self.title
